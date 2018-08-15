8093dc932d5f86338a763c6440600573349a6c89
Olha Melska <olya.melska@gmail.com>
2018-08-15 00:00:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function destructivelyPrependDriver(name){
 }
 
 function destructivelyRemoveLastDriver(){
-  return drivers.shift(2);
+  return drivers.shift(1);
 }
 
 function destructivelyRemoveFirstDriver(){
